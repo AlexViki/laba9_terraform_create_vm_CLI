cd45a6e16e18d0497dd6738cde1e04ad1cbbdeaa
Alex <shevashokc@gmail.com>
2020-03-22 16:34:18 +0200

    Update main.tf

--- vm/main.tf
+++ vm/main.tf
@@ -1,14 +1,10 @@
-provider "azurerm" {
-  features {}
-}
-
 variable "prefix" {
   default = "laba-9"
 }
 
 resource "azurerm_resource_group" "main" {
   name     = "${var.prefix}-resources"
-  location = "westeurope"
+  location = "West US 2"
 }
 
 resource "azurerm_virtual_network" "main" {
@@ -27,40 +23,44 @@ resource "azurerm_subnet" "internal" {
 
 resource "azurerm_network_interface" "main" {
   name                = "${var.prefix}-nic"
-  resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
 
   ip_configuration {
-    name                          = "internal"
+    name                          = "testconfiguration1"
     subnet_id                     = azurerm_subnet.internal.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
-resource "azurerm_linux_virtual_machine" "main" {
-  name                            = "${var.prefix}-vm"
-  resource_group_name             = azurerm_resource_group.main.name
-  location                        = azurerm_resource_group.main.location
-  size                            = "Standard_B1s"
-  admin_username                  = "alex"
-  network_interface_ids = [
-    azurerm_network_interface.main.id,
-  ]
+resource "azurerm_virtual_machine" "main" {
+  name                  = "${var.prefix}-vm"
+  location              = azurerm_resource_group.main.location
+  resource_group_name   = azurerm_resource_group.main.name
+  network_interface_ids = [azurerm_network_interface.main.id]
+  vm_size               = "Standard_B1s"
 
-  admin_ssh_key {
-    username = "alex"
-    public_key = file("~/.ssh/terraform_vm.pub")
-  }
-
-  source_image_reference {
+  storage_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
     sku       = "18.04-LTS"
     version   = "latest"
   }
-
-  os_disk {
-    storage_account_type = "Standard_LRS"
-    caching              = "ReadWrite"
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "${var.prefix}-vm"
+    admin_username = "alex"
+    admin_password = "Password1234!"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+    environment = "staging"
   }
 }
